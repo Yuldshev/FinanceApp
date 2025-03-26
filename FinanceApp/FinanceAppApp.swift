0b18f6b3079076082204a011ee1232dbb8d731e0
Yuldashev <ikrom921@gmail.com>
2025-03-26 10:43:27 +0500

    Initial Commit

---
+++ FinanceApp/FinanceAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinanceAppApp.swift
+//  FinanceApp
+//
+//  Created by Yuldashev Fazliddin on 26/03/25.
+//
+
+import SwiftUI
+
+@main
+struct FinanceAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
